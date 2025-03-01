137edc78614acd2b49a26fdb71c323bc6a7043a5
BETTERGanking <nsagexb@gmail.com>
2025-03-01 21:30:01 +0000

    dsa

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+     
+
+      <div className="video-wrapper">
+        <iframe 
+          src="https://www.youtube.com/embed/D_MUbs9dnDU" 
+          title="Watch for a free rank"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+        <p>WWHY thelllll</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
